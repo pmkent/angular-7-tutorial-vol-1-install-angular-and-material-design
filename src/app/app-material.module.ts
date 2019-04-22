d252a5a5e6c161490e20c2a82dbc82e01a1643aa
Phil Kieti <philmkieti@hotmail.com>
2019-04-22 19:39:49 -0400

    First push into the new GitHub repo

---
+++ src/app/app-material.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import * as mat from '@angular/material';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    mat.MatToolbarModule
+  ],
+  exports: [
+    mat.MatToolbarModule
+  ]
+})
+export class AppMaterialModule { }
